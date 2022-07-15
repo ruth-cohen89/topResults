60680e78c65ee441c1b27a9b33ec3930ef4dc12c
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-07-16 00:15:21 +0300

    db

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const mysql = require('mysql');
 const app = express();
 
 const db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '1234',
-  database: 'top_scores'
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'top_scores',
 });
 
 db.connect((err) => {
@@ -24,22 +24,30 @@ const createDB = () => {
     if (err) throw err;
     console.log('error connecting to db: ', result);
   });
-}
+};
 
 const createUsersTable = () => {
-  let sql = 'CREATE TABLE Users(id int AUTO_INCREMENT, userName VARCHAR(255) UNIQUE, FirstName VARCHAR(255), PRIMARY KEY (id))';
-  
+  let sql =
+    'CREATE TABLE Users(id int AUTO_INCREMENT, userName VARCHAR(255) UNIQUE NOT NULL, fullName VARCHAR(255) NOT NULL, PRIMARY KEY (id))';
+
   db.query(sql, (err, result) => {
-    if(err) throw err; 
+    if (err) throw err;
     console.log(result);
   });
-}
+};
 
 const createTopScoresTable = () => {
-  let sql = 'CREATE TABLE Scores(id int AUTO_INCREMENT, userName varchar(255), game varchar(255) UNIQUE, PRIMARY KEY (id))';
+  let sql =
+    'CREATE TABLE scores(id int AUTO_INCREMENT, userName varchar(255) NOT NULL, game varchar(255) NOT NULL, score varchar(255) NOT NULL, PRIMARY KEY (id))';
   
   db.query(sql, (err, result) => {
-    if(err) throw err; 
+    if (err) throw err;
+    console.log(result);
+  });
+  sql = 'ALTER TABLE scores ADD UNIQUE unique_index(userName, game)';
+
+  db.query(sql, (err, result) => {
+    if (err) throw err;
     console.log(result);
   });
 };
@@ -47,41 +55,63 @@ const createTopScoresTable = () => {
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));//?
+
+// app.use(express.urlencoded({ extended: true, limit: '10kb' })); //?
+app.use(express.json());
 
 app.post('/adduser', (req, res) => {
-  let user = {userName:req.body.userName, firstName:req.body.firstName}
-  let sql = 'INSERT INTO users SET ?'
-  db.query(sql, (err, result) => {
-    if(err) throw err; 
-    console.log(result);
-    res.send(`${req.body.userName} successfuly added`);
-  });
-});
- 
-app.post('/submit', (req, res) => {
-  let score = {userName:req.body.userName, firstName:req.body.firstName}
-  let sql = 'INSERT INTO scores SET ?'
-  db.query(sql, (err, result) => {
-    if(err) throw err; 
-    console.log(result);
-    res.send(`${req.body.userName} successfuly added`);
+  let user = { userName: req.body.userName, fullName: req.body.fullName };
+  let sql = 'INSERT INTO users SET ?';
+
+  db.query(sql, user, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    };
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: result,
+      },
+    });
   });
 });
 
-// app.get('/adduser', (req, res) => {
-//   let user = {userName:req.body.userName, firstName:req.body.email}
-//   db.query(sql, (err, result) => {
-//     if(err) throw err; 
-//     console.log(result);
-//     res.send('Scores table created...');
-//   });
-// });
+app.post('/submit', (req, res) => {
+  let sql = 
+  let score = { userName: req.body.userName, game: req.body.game, score: req.body.score };
+  let sql = `SELECT score FROM scores WHERE userName=${score.userName} AND game=${score.game} AND score<${score.score}`;
+  db.query
+  if()
+  sql = 'INSERT INTO scores SET ?';
+
+  db.query(sql, score, (err, result) => {
+    if (err) {
+      console.log(err);
+      if (err.errno === 1062) {
+        res.send('User name already exits, please choose another one.');
+      }
+      if (err.errno === 1) {
+        res.send('User name already exits, please choose another one.');
+      }
+    }
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: result,
+      },
+    });
+  });
+});
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server 🙄`, 404));
 });
 
 app.use(globalErrorHandler);
-createTopScoresTable()
+
+createUsersTable();
+createTopScoresTable();
 module.exports = app;
