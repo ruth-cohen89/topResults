192921eab8210a7f9cbff08239b215f91b8b349a
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-07-15 22:18:51 +0300

    new db

--- app.js
+++ app.js
@@ -1,39 +1,87 @@
 const express = require('express');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-//const userRouter = require('./routes/userRoutes');
-//const path = require('path');
-//const compression = require('compression');
-
+const mysql = require('mysql');
 const app = express();
+
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1234',
+  database: 'top_scores'
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySql Connected...');
+});
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
-//app.use(cookieParser());
-//app.use(compression());
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));//?
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
+let sql = 'CREATE DATABASE top_scores';
+
+db.query(sql, (err, result) => {
+  if (err) throw err;
+  console.log('error connecting to db: ', result);
+  res.send('Database created...');
 });
 
-app.use('/api', (req, res, next) => {
-  res.status(200).json({
-    status: 'success',
-    results: foods.length,
-    data: {
-      data: 'Welcome to top-results API',
-    },
+
+let sql = 'CREATE TABLE Users(id int AUTO_INCREMENT, userName VARCHAR(255) UNIQUE, FirstName VARCHAR(255), PRIMARY KEY (id))';
+db.query(sql, (err, result) => {
+  if(err) throw err; 
+  console.log(result);
+  res.send('Users table created...');
+});
+
+app.get('/createscorestable', (req, res) => {
+  let sql = 'CREATE TABLE Scores(id int AUTO_INCREMENT, userName varchar(255) UNIQUE, FirstName varchar(255), PRIMARY KEY (id))';
+  
+  db.query(sql, (err, result) => {
+    if(err) throw err; 
+    console.log(result);
+    res.status('Users table created...');
   });
 });
 
-//app.use('/api/v1/users', userRouter);
-// app.use('/api/v1/users', userRouter);
-// app.use('/api/v1/users', userRouter);
+app.post('/adduser', (req, res) => {
+  let user = {userName:req.body.userName, firstName:req.body.firstName}
+  let sql = 'INSERT INTO users SET ?'
+  db.query(sql, (err, result) => {
+    if(err) throw err; 
+    console.log(result);
+    res.send(`${req.body.userName} successfuly added`);
+  });
+});
+ 
+app.post('/submit', (req, res) => {
+  let score = {userName:req.body.userName, firstName:req.body.firstName}
+  let sql = 'INSERT INTO scores SET ?'
+  db.query(sql, (err, result) => {
+    if(err) throw err; 
+    console.log(result);
+    res.send(`${req.body.userName} successfuly added`);
+  });
+});
 
+// app.get('/adduser', (req, res) => {
+//   let user = {userName:req.body.userName, firstName:req.body.email}
+//   db.query(sql, (err, result) => {
+//     if(err) throw err; 
+//     console.log(result);
+//     res.send('Scores table created...');
+//   });
+// });
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server 🙄`, 404));
@@ -41,5 +89,3 @@ app.all('*', (req, res, next) => {
 
 app.use(globalErrorHandler);
 module.exports = app;
-
- 
