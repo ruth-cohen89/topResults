ae0060696b3cc77857ddb92cf49f550c85294c0b
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-07-15 22:20:20 +0300

    m

--- server.js
+++ server.js
@@ -1,49 +1,25 @@
 const dotenv = require('dotenv');
 const app = require('./app');
-const mysql = require('mysql');
 
 // Synchronous exeptions on the app
-process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION! ⛔ Shutting down...');
-  console.log(err.name, err);
-  process.exit(1);
-});
+// process.on('uncaughtException', (err) => {
+//   console.log('UNCAUGHT EXCEPTION! ⛔ Shutting down...');
+//   console.log(err.name, err);
+//   process.exit(1);
+// });
 
 dotenv.config({ path: './config.env' });
-
-const db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '123456'
-});
-
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log('MySql Connected...');
-});
-
 const port = process.env.PORT;
 
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-app.get('/createdb', (req, res) => {
-  let sql = 'CREATE DATABASE nodemysql';
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log('error connecting to db: ', result);
-    res.send('Database created...');
-  });
-});
-
 // Promise rejections (outside of the app)
-process.on('unhandledRejection', (err) => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// process.on('unhandledRejection', (err) => {
+//   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+//   console.log(err.name, err.message);
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
