382260be6f2ab5a93a084bff4442bcd84f631165
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-07-16 17:48:14 +0300

    s

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const createUsersTable = () => {
 const createTopScoresTable = () => {
   let sql =
     'CREATE TABLE scores(id int AUTO_INCREMENT, userName varchar(255) NOT NULL, game varchar(255) NOT NULL, score varchar(255) NOT NULL, PRIMARY KEY (id))';
-  
+
   db.query(sql, (err, result) => {
     if (err) throw err;
     console.log(result);
@@ -56,7 +56,6 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// app.use(express.urlencoded({ extended: true, limit: '10kb' })); //?
 app.use(express.json());
 
 app.post('/adduser', (req, res) => {
@@ -66,35 +65,10 @@ app.post('/adduser', (req, res) => {
   db.query(sql, user, (err, result) => {
     if (err) {
       console.log(err);
-      res.send(err);
-    };
-
-    res.status(201).json({
-      status: 'success',
-      data: {
-        data: result,
-      },
-    });
-  });
-});
-
-app.post('/submit', (req, res) => {
-  let sql = 
-  let score = { userName: req.body.userName, game: req.body.game, score: req.body.score };
-  let sql = `SELECT score FROM scores WHERE userName=${score.userName} AND game=${score.game} AND score<${score.score}`;
-  db.query
-  if()
-  sql = 'INSERT INTO scores SET ?';
-
-  db.query(sql, score, (err, result) => {
-    if (err) {
-      console.log(err);
       if (err.errno === 1062) {
-        res.send('User name already exits, please choose another one.');
-      }
-      if (err.errno === 1) {
-        res.send('User name already exits, please choose another one.');
-      }
+        return res.send('User name already exits, please choose another one.');
+      } 
+      return res.send('ERROR, Couldn\'t process your request ')
     }
 
     res.status(201).json({
@@ -106,12 +80,65 @@ app.post('/submit', (req, res) => {
   });
 });
 
+app.post('/submit', (req, res) => {
+  let score = {
+    userName: req.body.userName,
+    game: req.body.game,
+    score: req.body.score,
+  };
+
+  let sql = `SELECT score FROM scores WHERE userName='${score.userName}' AND game='${score.game}'`;
+  db.query(sql, (err, result) => {
+    console.log('result:', result, result.length > 0);
+    if (err) console.log(err);
+    if (result.length > 0 && result[0].score >= score.score) {
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          data: result,
+        },
+        message: `Your highest score is ${result[0].score}`,
+      });
+    }
+    sql = '';
+    if (result.length > 0) {
+      sql = `DELETE FROM scores WHERE userName='${score.userName}' AND game='${score.game}';`; 
+      db.query(sql, score, (err, result) => {
+        if (err) {
+          console.log(err);
+          let message;
+          if (err.errno === 1048) message = 'Error, please fill in userName'
+          return res.status(400).json({
+            status: 'client error',
+            
+            message: `Your highest score is ${result[0].score}`,
+          });
+        }
+      });
+    }
+    sql = 'INSERT INTO scores SET ?;';
+    db.query(sql, score, (err, result) => {
+      if (err) {
+        console.log(err);
+
+      }
+
+      res.status(201).json({
+        status: 'success',
+        data: {
+          data: result,
+        },
+      });
+    });
+  });
+});
+
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server 🙄`, 404));
 });
 
 app.use(globalErrorHandler);
 
-createUsersTable();
-createTopScoresTable();
+// createUsersTable();
+// createTopScoresTable();
 module.exports = app;
